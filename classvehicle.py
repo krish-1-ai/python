b2dba03b6c15805ce4e59a72b62ee6bb94481b23
krish-1-ai <nlihala@gmail.com>
2025-03-13 18:12:14 +0400

    Add files via upload

---
+++ classvehicle.py
@@ -0,0 +1,11 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+            self.max_speed = max_speed
+            self.mileage = mileage
+
+
+vehicleobjectone = vehicle(240, 18)
+
+print("Model max speed:",vehicleobjectone.max_speed)
+print("Model mileage:", vehicleobjectone.mileage)
+
